f8dc46c667ffc740bebaa0d43bc90be598c5b542
QuangThai <nguyenquangthaib1@gmail.com>
2020-03-19 14:59:15 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    // sticky header
+    $(window).scroll(function (e) {
+        var height = $(this).scrollTop();
+        if (height > 150) {
+            $('.header').addClass("sticky");
+        } else {
+            $('.header').removeClass("sticky");
+        }
+    });
+
+    // scroll Top
+    var scrollTop = $(".back-to-top");
+
+    $(window).scroll(function () {
+  
+      var topPos = $(this).scrollTop();
+      if (topPos > 100) {
+        $(scrollTop).css("opacity", "1");
+        $(scrollTop).css("visibility", "visible");
+  
+      } else {
+        $(scrollTop).css("opacity", "0");
+        $(scrollTop).css("visibility", "hidden");
+      }
+    });
+  
+    $(scrollTop).click(function () {
+      $('html, body').animate({
+        scrollTop: 0
+      }, 800);
+      return false;
+    });
+});
