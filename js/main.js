67e89057782f987fac8556562f5514f69dd68773
QuangThai <nguyenquangthaib1@gmail.com>
2020-03-20 10:26:14 +0700

    commit lần 2

--- js/main.js
+++ js/main.js
@@ -1,35 +1,39 @@
 $(document).ready(function () {
 
-    // sticky header
-    $(window).scroll(function (e) {
-        var height = $(this).scrollTop();
-        if (height > 150) {
-            $('.header').addClass("sticky");
-        } else {
-            $('.header').removeClass("sticky");
-        }
-    });
 
-    // scroll Top
-    var scrollTop = $(".back-to-top");
+  // sticky header
+  // $(window).scroll(function (e) {
+  //   var height = $(this).scrollTop();
+  //   if (height > 150) {
+  //     $('.header').addClass("header--sticky");
+  //   } else {
+  //     $('.header').removeClass("header--sticky");
+  //   }
+  // });
 
-    $(window).scroll(function () {
-  
-      var topPos = $(this).scrollTop();
-      if (topPos > 100) {
-        $(scrollTop).css("opacity", "1");
-        $(scrollTop).css("visibility", "visible");
-  
-      } else {
-        $(scrollTop).css("opacity", "0");
-        $(scrollTop).css("visibility", "hidden");
-      }
-    });
-  
-    $(scrollTop).click(function () {
-      $('html, body').animate({
-        scrollTop: 0
-      }, 800);
-      return false;
-    });
+  // scroll Top
+  var scrollTop = $(".back-to-top");
+
+  $(window).scroll(function () {
+
+    var topPos = $(this).scrollTop();
+    if (topPos > 100) {
+      $(scrollTop).css("opacity", "1");
+      $(scrollTop).css("visibility", "visible");
+
+    } else {
+      $(scrollTop).css("opacity", "0");
+      $(scrollTop).css("visibility", "hidden");
+    }
+  });
+
+  $(scrollTop).click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+
+  });
+
+
 });
