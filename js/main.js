7e169b980023bff2d565442280f0a75a63eea154
QuangThai <nguyenquangthaib1@gmail.com>
2020-03-22 23:45:54 +0700

    commit lần 8

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
     focusOnSelect: true,
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 3 } },
-      { breakpoint: 991, settings: { slidesToShow: 2 } },
+      { breakpoint: 991, settings: { slidesToShow: 3 } },
       { breakpoint: 730, settings: { slidesToShow: 2 } },
     ]
   });
@@ -47,7 +47,7 @@ $(document).ready(function () {
     responsive: [
       { breakpoint: 1024, settings: { slidesToShow: 5 } },
       { breakpoint: 991, settings: { slidesToShow: 3 } },
-      { breakpoint: 767, settings: { slidesToShow: 3 } },
+      { breakpoint: 767, settings: { slidesToShow: 2 } },
       { breakpoint: 479, settings: { slidesToShow: 2 } }
     ]
   });
